10dd67c4f4dcb8c26a6822b1f04b8e956204e0b6
KadinMeyer23 <vq4717wg@go.minnstate.edu>
2025-02-12 18:24:29 -0600

    Add files via upload

---
+++ Vins Fletcher Arrows/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Vins_Fletcher_Arrows
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Arrow aArrow = new Arrow(Arrowhead.obsidian,Fletching.goose,76);
+            Console.WriteLine("Your Total Cost:"+ aArrow.cost());
+        }
+    }
+}
